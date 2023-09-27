3704c57f464e15070ed7433e893817b528f9f8bf
Dxdroast <144261564+Dxdroast@users.noreply.github.com>
2023-09-27 22:30:47 +0530

    Add files via upload

---
+++ INTRO TO PROGRAMMING/AGE CALULATOR.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+	int age;
+	printf("ENTER THE AGE :");
+	scanf("%d", &age);
+	if(age>=18 ){
+		if(age<=100){
+		
+		printf("YOU ARE ELIGIBLE TO CAST VOTE ");
+	}
+	else{
+		printf("YOU ARE DEAD ");
+	}
+	}
+	else{
+		printf("YOU ARE DEAD");
+	}
+
+	}
+	
