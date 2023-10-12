c01846c1129de34058c96c81dead95e7b00ddca7
Samim Akhtar <144261564+Dxdroast@users.noreply.github.com>
2023-10-13 00:37:10 +0530

    Update area of triangle herons.c

--- INTRO TO PROGRAMMING/area of triangle herons.c
+++ INTRO TO PROGRAMMING/area of triangle herons.c
@@ -4,14 +4,14 @@
 int main(){
 	double a,b,c,s,area;
 	printf("ENTER THE LENGTHS OF FIRST SIDE :\n");
-	scanf("%lf,&a");
+	scanf("%lf",&a);
 	printf("ENTER THE LENGTHS OF SECOND SIDE :\n");
-	scanf("%lf,&b");
+	scanf("%lf",&b);
 	printf("ENTER THE LENGTHS OF THIRD  SIDE :\n");
-	scanf("%lf,&C");
+	scanf("%lf",&c);
 	//calculate the area of semi parameter
 	s=(a+b+c)/2;
 	area=sqrt(s*(s-a)*(s-b)*(s-c));
-	printf("THE AREA OF TRIANGLE USING HERON'S FORMULA IS : %Lf",area);
+	printf("THE AREA OF TRIANGLE USING HERON'S FORMULA IS : %lf",area);
 	return 0;
 }
